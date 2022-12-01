50ea2fe43dec8b36e63adc7cf25b431e54911836
DY <p930601@gmail.com>
2022-12-01 15:39:38 +0700

    hw

--- routes/comment.js
+++ routes/comment.js
@@ -6,8 +6,9 @@ const posts = require("../schemas/post.js")
 
 // [댓글 작성 API]
 router.post("/comments/:postId", async (req, res) => {
-const {postId} = req.params;
-const { commentContents, commentPassword, user } = req.body;
+try{
+  const {postId} = req.params;
+  const { commentContents, commentPassword, user } = req.body;
   // console.log(postId)
 
   const findPost = await posts.findOne({postId: postId})
@@ -21,13 +22,17 @@ const { commentContents, commentPassword, user } = req.body;
   }
   await comments.create({ commentContents, commentPassword, user, postId });
   return res.json({"message":"댓글을 작성하였습니다."});
+}catch(error){
+res.status(500).json({error})
+}
 });
 
 
 // [댓글 목록 조회 API]
 router.get("/comments/:postId", async (req, res) => {
+try{
   const {postId} = req.params;
-  const list = await comments.find({postId: postId});
+  const list = await comments.find({postId: postId}).sort({createdAt: -1});
   const findPost = await posts.findOne({postId: postId})
 
   if (findPost === null) {
@@ -43,49 +48,60 @@ router.get("/comments/:postId", async (req, res) => {
       };
     });
     res.json({ data });  
-  });
+}catch(error){
+res.status(500).json({error})
+}
+});
 
 
 // [댓글 수정 API]
 router.put("/comments/:commentId", async(req,res) => {
-    const {commentId} = req.params;
-    const commentPassword = req.body.commentPassword;
-    const commentContents = req.body.commentContents;
-    const findComment = await comments.findOne({commentId});
+try{
+  const {commentId} = req.params;
+  const commentPassword = req.body.commentPassword;
+  const commentContents = req.body.commentContents;
+  const findComment = await comments.findOne({commentId});
 
-    // console.log(findComment)
-    if (findComment === null) {
-      return res.status(404).json({"messageError":"댓글을 찾을 수 없습니다."});
-    }
-    if (commentPassword !== findComment.commentPassword) {
-      return res.status(400).json({
-      "errorMessage": "댓글 수정에 실패하였습니다."});
-    };
-    if (commentContents === undefined || commentContents === "") {
-      return res.status(400).json({"messageError":"댓글을 입력해주세요."});
-    }
-    await comments.updateOne({ commentId: Number(commentId) }, { $set: req.body });
-    res.status(200).json({"message":"댓글 수정하였습니다."});
+  // console.log(findComment)
+  if (findComment === null) {
+    return res.status(404).json({"messageError":"댓글을 찾을 수 없습니다."});
+  }
+  if (commentPassword !== findComment.commentPassword) {
+    return res.status(400).json({
+    "errorMessage": "댓글 수정에 실패하였습니다."});
+  };
+  if (commentContents === undefined || commentContents === "") {
+    return res.status(400).json({"messageError":"댓글을 입력해주세요."});
+  }
+  await comments.updateOne({ commentId: Number(commentId) }, { $set: req.body });
+  res.status(200).json({"message":"댓글 수정하였습니다."});
+}catch(error){
+res.status(500).json({error})
+}
 });
 
 
 // [게시글 삭제 API]
 router.delete("/comments/:commentId", async(req,res) => {
-    const {commentId} = req.params;
-    const commentPassword = req.body.commentPassword;
-    const findComment = await comments.findOne({commentId});
+try{
+  const {commentId} = req.params;
+  const commentPassword = req.body.commentPassword;
+  const findComment = await comments.findOne({commentId});
 
-    if (findComment === null) {
-      return res.status(404).json({"messageError":"댓글을 찾을 수 없습니다."});
-    }
-    if (commentPassword !== findComment.commentPassword) {
-      return res.status(400).json({
-      "errorMessage": "댓글 삭제에 실패하였습니다."});
-    }
+  if (findComment === null) {
+    return res.status(404).json({"messageError":"댓글을 찾을 수 없습니다."});
+  }
+  if (commentPassword !== findComment.commentPassword) {
+    return res.status(400).json({
+    "errorMessage": "댓글 삭제에 실패하였습니다."});
+  }
 
-    await comments.deleteOne({commentId});
-    return res.status(200).json({"message":"댓글을 삭제하였습니다."});
-  });
+  await comments.deleteOne({commentId});
+  return res.status(200).json({"message":"댓글을 삭제하였습니다."});
+}catch(error){
+res.status(500).json({error})
+}
+});
 
 
 module.exports = router;
--- routes/posts.js
+++ routes/posts.js
@@ -3,65 +3,73 @@ const router = express.Router();
 const posts = require("../schemas/post.js");
 
 
-
 // [게시글 작성 API]
 router.post("/posts", async (req, res) => {
-
+try{
   // const postsDate = new Date(postsDate);
 	const { title, name, password, contents } = req.body;
 
   await posts.create({ title, name, password, contents });
   res.json({"message":"게시글을 작성하였습니다."});
+}catch(error){
+res.status(500).json({error})
+}
 });
 
 
 
 // [전체 게시글 목록 조회 API]
 router.get("/posts", async (req, res) => {
-  const list = await posts.find();
-    const data = list.map((post) => {
-		return {
-      postId: post.postId,  //post 폴더에 저장된거
-      user: post.name,   
-      title: post.title,
-      createdAt: post.createdAt
-		};
+try{
+  const list = await posts.find().sort({createdAt: -1})
+  const data = list.map((post) => {
+	return {
+    postId: post.postId,  //post 폴더에 저장된거
+    user: post.name,   
+    title: post.title,
+    createdAt: post.createdAt
+    };
   });
-
   res.json({ data });
-  });
+}catch(error){
+res.status(500).json({error})
+}
+});
 
 
 
 // [게시글 상세 조회 API]
 router.get("/posts/:postId", async (req, res) => {
-    const {postId} = req.params;
-    const detail = await posts.find({postId:postId});
-          // console.log(detail)
+try{
+  const {postId} = req.params;
+  const detail = await posts.find({postId:postId});
+    // console.log(detail)
     if (detail.length === 0) {
       return res.status(400).json({
         "errorMessage": "데이터가 형식이 올바르지 않습니다."});
     };
-
-    const data = detail.map((post) => {
-      return {
-        postId: post.postId,
-        user: post.name,   
-        title: post.title,
-        content: post.contents,
-        createdAt: post.createdAt
-      };
-    });
-
-    res.json({data});
+  const data = detail.map((post) => {
+    return {
+      postId: post.postId,
+      user: post.name,   
+      title: post.title,
+      content: post.contents,
+      createdAt: post.createdAt
+    };
+  });
+  res.json({data});
+}catch(error){
+res.status(500).json({error})
+}
 });
 
 
 // [게시글 수정 API]
 router.put("/posts/:postId", async(req,res) => {
-    const {postId} = req.params;
-    const password = req.body.password;
-    const findPosts = await posts.findOne({postId});
+try{
+  const {postId} = req.params;
+  const password = req.body.password;
+  const findPosts = await posts.findOne({postId});
 
   if (password !== findPosts.password) {
     return res.status(400).json({
@@ -70,12 +78,16 @@ router.put("/posts/:postId", async(req,res) => {
 
   await posts.updateOne({ postId: Number(postId) }, { $set: req.body });
   res.status(200).json({"message":"게시글을 수정하였습니다."});
+}catch(error){
+res.status(500).json({error})
+}
 });
 
 
 
 // [게시글 삭제 API]
 router.delete("/posts/:postId", async(req,res) => {
+try{
   const {postId} = req.params;
   const password = req.body.password;
   const findPosts = await posts.findOne({postId});
@@ -91,7 +103,10 @@ router.delete("/posts/:postId", async(req,res) => {
 
   await posts.deleteOne({ postId: Number(postId) }); 
   res.status(200).json({"message":"게시글을 삭제하였습니다."});
-  });
+}catch(error){
+  res.status(500).json({error})
+}
+});
 
 
 
